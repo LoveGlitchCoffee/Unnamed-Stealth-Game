f1d598104c75ee9c17131850fec27f75ce2402b0
ParityB1t <hung.h.progdev@gmail.com>
2015-03-15 22:41:14 +0000

    first commit, has physics2D working

---
+++ Assets/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    KeyCode goLeft = KeyCode.A;
+    KeyCode goRight = KeyCode.D;
+    int speed = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKey(goLeft))
+        {
+            rigidbody2D.AddForce(new Vector2(-speed, 0));
+        }
+        else if (Input.GetKey(goRight))
+        {
+            rigidbody2D.AddForce(new Vector2(speed, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.AddForce(new Vector2(0, 230));
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+
+        }
+	}
+}
---
+++ Assets/SlaverBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlaverBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionStay2D(Collision2D collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "Slave")
+        {
+            Debug.Log("contact");
+        }
+    }
+}
