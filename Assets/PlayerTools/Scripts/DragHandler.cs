56fcabcedb496526b7ee1291627f0c0a5f4ff820
ParityB1t <hung.h.progdev@gmail.com>
2015-04-21 15:43:18 +0100

    fixed tooltip, implementing drag drop

--- Assets/PlayerTools/Scripts/DragHandler.cs
+++ Assets/PlayerTools/Scripts/DragHandler.cs
@@ -5,16 +5,25 @@ using UnityEngine.EventSystems;
 public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
-    private GameObject slotOfItem;
+    private Tool _itemBeingDragged;
+    private Vector3 _startPosition;
+    public GameObject InteractiveObj;
+    private GameObject _physicalSpawn;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        slotOfItem = gameObject.transform.parent.gameObject;
+        _itemBeingDragged = gameObject.transform.parent.gameObject.GetComponent<ItemUI>().ReturnToolInSlot();
+        _startPosition = transform.position;
+
+        _physicalSpawn = Instantiate(InteractiveObj);
+        _physicalSpawn.GetComponent<Identifer>().SetIdentity(_itemBeingDragged);
+        _physicalSpawn.transform.position.Set(_startPosition.x, _startPosition.y, _startPosition.z);
+
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-    
+        _physicalSpawn.transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
---
+++ Assets/PlayerTools/Scripts/Identifer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Identifer : MonoBehaviour
+{
+
+    private Tool _identity;
+
+
+    public void SetIdentity(Tool identityTool)
+    {
+        _identity = identityTool;
+        SetImage();
+    }
+
+    private void SetImage()
+    {
+        
+    }
+
+    public Tool GetIdentity()
+    {
+        return _identity;
+    }
+}
