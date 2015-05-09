d7cf455eb24b84da0eee3a0a9537f6b66615f421
ParityB1t <hung.h.progdev@gmail.com>
2015-05-09 09:51:09 +0100

    light to highlight objects

---
+++ Assets/Scripts/EnviScripts/ShineEffect.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShineEffect : MonoBehaviour
+{
+
+    private Light _shineLight;
+    private float normalLighting = 0.5f;
+    private float highLighting = 4f;
+
+    void Awake()
+    {
+        _shineLight = GetComponent<Light>();
+    }
+
+	void Start ()
+	{
+	    StartCoroutine(ShineLight());
+	}
+
+    private IEnumerator ShineLight()
+    {
+        bool shine = true;
+        float timeStamp = Time.time;
+        
+        float secondLighting = normalLighting;
+        float firstLighting = highLighting;
+
+        while (shine)
+        {
+            if (Time.time - timeStamp > 1f)
+                timeStamp = Time.time;
+
+            _shineLight.intensity = Mathf.Lerp(firstLighting, secondLighting, (Time.time-timeStamp)/1.7f);
+
+            if (_shineLight.intensity == highLighting)
+            {
+                firstLighting = highLighting;
+                secondLighting = normalLighting;
+            }
+            else if (_shineLight.intensity == normalLighting)
+            {
+                firstLighting = normalLighting;
+                secondLighting = highLighting;
+            }
+
+            yield return null;
+        }
+           
+            
+            
+        
+    }
+}
