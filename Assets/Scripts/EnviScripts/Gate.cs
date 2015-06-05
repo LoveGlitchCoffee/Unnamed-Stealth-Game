d59bd1413cf7a1f91371e509b6fdbf09a5830bc3
ParityB1t <hung.h.progdev@gmail.com>
2015-06-05 14:30:36 +0100

    reveretd to pre-B trip and re-added lever and gate

---
+++ Assets/Scripts/EnviScripts/Gate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gate : MonoBehaviour, IInteractive
+{
+
+    private Animator _anim;
+    [HideInInspector]
+    public bool CanOpen;
+
+
+    public void PerformPurpose(InventoryLogic inventory)
+    {
+        if (CanOpen)
+            StartCoroutine(Open());
+    }
+
+    public IEnumerator Open()
+    {
+
+        GetComponent<BoxCollider2D>().enabled = false;
+
+        while (transform.localScale.y > 0)
+        {
+            transform.localScale -= new Vector3(0, 0.2f);
+            transform.position += new Vector3(0, 0.05f);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/EnviScripts/Lever.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : MonoBehaviour, IInteractive
+{
+
+    private GameObject _gate;
+    public string GateName;
+    private Animator _anim;
+
+    //extendible to anything, not just opening gates, keep <IInteractive>
+    void Awake()
+    {
+        GameObject[] gates = GameObject.FindGameObjectsWithTag("Gates");
+
+        for (int i = 0; i < gates.Length; i++)
+        {
+            if (gates[i].name == GateName)
+            {
+                _gate = gates[i];
+                break;
+            }
+        }
+
+        _anim = GetComponent<Animator>();
+    }
+
+
+    /**haven't thought of way around this
+     */
+    public void PerformPurpose(InventoryLogic inventory)
+    {
+        _anim.SetBool("Activated", true);
+        _gate.GetComponent<Gate>().CanOpen = true;
+        _gate.GetComponent<IInteractive>().PerformPurpose(inventory);
+
+    }
+
+}
